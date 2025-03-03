57f821fe6f72951844f51c819fcc5e431056a72d
Buttwood <wjt666wjt@163.com>
2025-03-03 11:30:26 +0800

    style:better mobile

--- www/js/china-map.js
+++ www/js/china-map.js
@@ -1,18 +1,40 @@
 
-    var chinaMap = echarts.init(document.getElementById("china-map"));
-    window.onresize = chinaMap.resize; // 窗口或框架被调整大小时执行chinaMap.resize
-    chinaMap.setOption({
+var chinaMap = echarts.init(document.getElementById("china-map"));
+window.onresize = chinaMap.resize; // 窗口或框架被调整大小时执行chinaMap.resize
+const leftBox = document.getElementById("left-box");
+
+// 监听 leftBox 宽度变化
+const resizeObserver = new ResizeObserver(() => {
+    chinaMap.resize();
+});
+resizeObserver.observe(leftBox);
+window.addEventListener("resize", function () {
+    chinaMap.resize();
+});
+chinaMap.setOption({
     // 进行相关配置
-    tooltip: {}, // 鼠标移到图里面的浮动提示框
+    tooltip: {
+        formatter: function (params) {
+            // 将value转换为数字显示
+            let value = params.value.toString();
+            if (!isNaN(value)) {
+                let year = value.substring(0, 4);
+                let month = value.substring(4, 6);
+                let day = value.substring(6, 8);
+                value = `${year}-${month}-${day}`;
+            }
+            return params.name + ": " + value;
+        }
+    },
     dataRange: {
-    show: false,
-    min: 0,
-    max: 1000,
-    text: ["High", "Low"],
-    realtime: true,
-    calculable: true,
-    color: ["orangered", "#FF9B52", "#FFD068"],
-},
+        show: false,
+        min: 0,
+        max: 9999999999,
+        text: ["High", "Low"],
+        realtime: true,
+        calculable: true,
+        color: ["orangered", "#FF9B52", "#FFD068"],
+    },
     geo: {
     // 这个是重点配置区
     map: "china", // 表示中国地图
@@ -47,7 +69,7 @@
     coordinateSystem: "geo", // 对应上方配置
 },
 {
-    name: "照片张数", // 浮动框的标题
+    name: "点亮日期", // 浮动框的标题
     type: "map",
     geoIndex: 0,
     data: data
